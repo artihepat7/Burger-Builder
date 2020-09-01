59c6455690b5adb3cd580746d8f3531654583f9d
artihepat7 <hepatarti@gmail.com>
2020-09-01 15:33:30 +0530

    Burger Builder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,32 @@
 import Aux from "../../hoc/Auxiliary";
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
-const layout = (props) => {
-  return (
-    <Aux>
-      <Toolbar />
-      <main className={classes.Content}>{props.children}</main>
-    </Aux>
-  );
-};
-export default layout;
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
+class Layout extends Component {
+  state = { SideDrawerShow: false };
+  SideDrawerClosedHandler = () => {
+    this.setState({ SideDrawerShow: false });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { SideDrawerShow: !prevState.SideDrawerShow };
+    });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          clicked={this.SideDrawerClosedHandler}
+          show={this.state.SideDrawerShow}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+export default Layout;
